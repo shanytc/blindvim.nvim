f2eabb5d80fb269b6c746bd5877ce32868575e77
Shany G <shany@pm.me>
2023-01-20 11:09:48 +0200

    added plugin folder with use commands

---
+++ plugin/init.lua
@@ -0,0 +1,53 @@
+vim.api.nvim_create_user_command("Blindvim",function(opts)
+    local cmd = opts.args
+
+    if cmd == "on" then
+        require("blindvim").start()
+        return
+    end
+
+    if cmd == "off" then
+        require("blindvim").stop()
+        return
+    end
+
+    if cmd == "flash" then
+        require("blindvim").mark()
+        return
+    end
+
+    if cmd == "reflash" then
+        require("blindvim").clear_marks()
+        return
+    end
+
+    if cmd == "unflash" then
+        require("blindvim").unmark()
+        return
+    end
+
+    if cmd == "hide" then
+        require("blindvim").mark_hidden()
+        return
+    end
+
+    if cmd == "unhide" then
+        require("blindvim").unmark_hidden()
+        return
+    end
+
+    if cmd == "blind" then
+        require("blindvim").blind()
+        return
+    end
+end, {
+    nargs="?",
+    complete = function ()
+        return {
+            "on","off",
+            "blind",
+            "flash", "unflash", "reflash",
+            "hide", "unhide"
+        }
+    end
+})
